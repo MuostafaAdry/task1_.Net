1f02d06989304e7836920b4ad16e3cf0aa52f2eb
MuostafaAdry <mistafaadry10@gmail.com>
2024-11-16 21:54:09 +0200

    update and finsh task 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,30 +1,114 @@
-﻿using System.Threading.Channels;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int smallCarpet = 25;
-            int largeCarpet = 35;
-            double saleTax = 0.06;
-            Console.WriteLine("enter the number of small carpit");
-            int numerSmallCarpit = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter the number of large carpit");
-            int numerLargelCarpit = int.Parse(Console.ReadLine());
-            //store the total in the var to add  saleTax
-            int Sum = (smallCarpet * numerSmallCarpit) + (largeCarpet * numerLargelCarpit);
-            Console.WriteLine($" the Sum is {Sum}");
-            //store the total after adding the saleTax
-            double Sum_saleTax = (Sum * saleTax);
+            List<int> MyList = new List<int>();
 
-            Console.WriteLine($" and the totla is  {Sum_saleTax + Sum } $ there is 6% for taxe");
+            if (MyList.Count == 0)
+            {
+                Console.WriteLine("Your list is empty.");
+            }
 
-            //ليه مظهرش علامه$ 
-            //Console.WriteLine($"Equation: {Sum_saleTax + Sum} = {Sum_saleTax + Sum : C}");
+            while (true)
+            {
+                Console.WriteLine("Enter even values (press 0 to stop):");
+                int listValues = int.Parse(Console.ReadLine());
+                // new feature for bouns ==> you con add even number only
+                if (listValues % 2 == 0)
+                {
+                  //to add any number != zero علشان مياثرش على average & min value
+                    if (listValues != 0)
+                    {
+                        MyList.Add(listValues);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter EVEN NUMBER ONLY. Try again.");
+                    MyList.Clear();
+                }
+                //handel min & max value and average
 
+                if (listValues == 0)
+                {
+                    if (MyList.Count > 0)
+                    {
+                        int minValue = MyList[0];
+                        int maxValue = MyList[0];
+                        double theAverage = 0.0;
+
+                        for (int i = 0; i < MyList.Count; i++)
+                        {
+                            Console.WriteLine($"The values in the list are ===> {MyList[i]}");
+                            theAverage +=  MyList[i] / MyList.Count;
+
+                            if (maxValue < MyList[i])
+                            {
+                                maxValue = MyList[i];
+                            }
+
+                            if (minValue > MyList[i])
+                            {
+                                minValue = MyList[i];
+                            }
+                        }
+
+                        Console.WriteLine("=========================================");
+                        Console.WriteLine($"The average is {theAverage}");
+                        Console.WriteLine("=========================================");
+                        Console.WriteLine($"The min value is {minValue}");
+                        Console.WriteLine("=========================================");
+                        Console.WriteLine($"The max value is {maxValue}");
+                    }
+                    //else
+                    //{
+                    //    Console.WriteLine("Your list is empty.");
+                    //}
+
+
+
+                    //handel search value
+                    Console.WriteLine("Enter the search value:");
+                    int userSearch = int.Parse(Console.ReadLine());
+                     
+
+                    for (int j = 0; j < MyList.Count; j++)
+                    {
+                        if (userSearch == MyList[j])
+                        {
+                            Console.WriteLine($"Your value already exists in the list ===> {userSearch} with index ===> {j}");
+                            
+                            break;
+                        }
+ 
+                        else if (userSearch % 2 != 0)
+                        {
+                            Console.WriteLine("Your search is an ODD number. This list accepts EVEN numbers only.");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Your value does NOT exist.");
+                            break;
+                        }
+                    }
+
+
+                    //handel delet values from list
+                    Console.WriteLine("Enter 'd' to delete all values:");
+                    string dletValues = Console.ReadLine();
+
+                    if (dletValues == "d")
+                    {
+                        MyList.Clear();
+                        Console.WriteLine("All values are deleted.");
+                    }
+
+                    break;
+                }
+            }
         }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d38964d66f658222603fa6755d342c4e91af6790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dfd34301502b0e953211a1f45c1b446dbbd8e82")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
